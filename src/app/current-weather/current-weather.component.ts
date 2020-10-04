57fff6b43e648da1e42e60359dec261fed88ba7c
Santosh Marigowda <santosharakere@gmail.com>
2020-10-04 06:06:17 +0100

    data binding and pipes in current-weather component

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ICurrentWeather } from '../interfaces'
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.scss']
+  styleUrls: ['./current-weather.component.scss'],
 })
 export class CurrentWeatherComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  current: ICurrentWeather
+  constructor() {
+    this.current = {
+      city: 'Bethesda',
+      country: 'US',
+      date: new Date(),
+      image: 'assets/img/sunny.webp',
+      temperature: 72,
+      description: 'Sunny',
+    }
   }
 
+  ngOnInit(): void {}
 }
