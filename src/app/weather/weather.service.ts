0393a978825f0011474e825dac97c0bccdd8ba9c
Santosh Marigowda <santosharakere@gmail.com>
2020-10-04 06:58:38 +0100

    current weather data interface

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpClientModule } from '@angular/common/http'
 
+interface ICurrentWeatherData {
+  weather: [
+    {
+      description: string
+      icon: string
+    }
+  ]
+  main: {
+    temp: number
+  }
+  sys: {
+    country: string
+  }
+  dt: number
+  name: string
+}
+
 @Injectable({
   providedIn: 'root',
 })
